40d7e93fd1bff09e4b71328daad0b17877274d0a
jchzener <35633462+jchzener@users.noreply.github.com>
2026-02-12 22:02:17 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -223,7 +223,7 @@ export default function Home() {
       <Section className="py-20 text-center">
         <div className="max-w-7xl mx-auto px-6">
           {/* <p className="text-stone-600 text-sm md:text-lg font-bold mb-4"> */}
-            {t("home.who.description")}
+            /* {t("home.who.description")} */
           </p>
 
           <h2 className="text-xl md:text-3xl font-semibold mb-6 whitespace-pre-line">
