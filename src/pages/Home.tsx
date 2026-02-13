fb4ffd63be3c11ec68984bce3ea8952a21c17893
jchzener <35633462+jchzener@users.noreply.github.com>
2026-02-12 22:01:36 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -222,7 +222,7 @@ export default function Home() {
       {/* FINAL CTA */}
       <Section className="py-20 text-center">
         <div className="max-w-7xl mx-auto px-6">
-          <p className="text-stone-600 text-sm md:text-lg font-bold mb-4">
+          {/* <p className="text-stone-600 text-sm md:text-lg font-bold mb-4"> */}
             {t("home.who.description")}
           </p>
 
